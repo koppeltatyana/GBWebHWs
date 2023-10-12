1c171c0981c96655ec93d6be63946526a8ee1cb7
koppel_tatyana <tatyana.koppel@mail.ru>
2023-10-12 14:40:37 +0300

    HW3

---
+++ HW3/js/main.js
@@ -0,0 +1,6 @@
+function greeting(name) {
+    console.log(`Привет, ${name}! ☺️`)
+
+    let p = document.getElementById("after_button").value = "Теперь можно открыть консоль";
+    document.getElementById("after_button").innerHTML = p;
+}
